8830626a178249ebf2fdf3ac73ecb2aa63b1ecb5
kiblitz <g.lee13770334@gmail.com>
2025-02-17 19:02:08 -0500

    mvp

--- rocml/bin/main.ml
+++ rocml/bin/main.ml
@@ -1,9 +1,17 @@
 open! Core
-open Ctypes
-open Foreign
 
-let lib = Dl.dlopen ~filename:"vectoradd_hip.so" ~flags:[ Dl.RTLD_NOW ]
-let f = foreign ~from:lib "f" (void @-> returning int)
+let[@inline] filepath_by_binary ~filename =
+  let bin_path = Sys_unix.executable_name |> Filename.parts |> List.drop_last_exn in
+  bin_path @ [ filename ] |> Filename.of_parts
+;;
+
+let lib =
+  Dl.dlopen
+    ~filename:(filepath_by_binary ~filename:"vectoradd_hip.so")
+    ~flags:[ Dl.RTLD_LAZY ]
+;;
+
+let f = Foreign.foreign ~from:lib "f" Ctypes.(void @-> returning int)
 
 let main () =
   [%my_ppx "yoo gabba gabba"];
